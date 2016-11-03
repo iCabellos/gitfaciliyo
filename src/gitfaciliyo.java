d986703fafd3c7e94779eb10f8711952d87ec1ce
Ivan <ivancabe.96@gmail.com>
2016-11-03 18:40:11 +0100

    Iniciamos

---
+++ src/gitfaciliyo.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Ragnarok on 3/11/16.
+ */
+public class gitfaciliyo {
+    public static void main(String[] args) {
+        printHola();
+    }
+    public static void printHola(){
+        System.out.println("Hello world!");
+    }
+}
